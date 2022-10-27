9072e321a9c94b1c3187466e13470a9854f583e2
Fransisco Leo Sinema Gea <202210715089@mhs.ubharajaya.ac.id>
2022-10-27 15:41:54 +0700

    Tugas sudah selesai
    Kirim

---
+++ Nomor_3.py
@@ -0,0 +1,14 @@
+#Nomor 3
+#contoh code if :
+umur = int(input('Masukkan umur: '))
+
+if umur >= 87:
+  print('Usia Pensiun')
+elif umur >= 55:
+  print('Usia Produktif')
+elif umur >= 24:
+  print('Usia Remaja')
+elif umur >= 7:
+  print('Usia Anak Anak')
+else:
+  print('kondisi salah ')
---
+++ Nomor_4.py
@@ -0,0 +1,16 @@
+#Nomor 4
+#contoh menggunakan pengulangan while :
+i = 1
+while i <= 5:
+  print(i)
+  i += 1
+
+#contoh pengulangan menggunakan for : 
+
+listAnimal = [
+  'Monkey', 'Crocodiles', 'Bird', 'Snake', 'Elephant',
+  'Ants', 'Cat', 'Lion'
+]
+
+for animal in listAnimal:
+  print(animal)
